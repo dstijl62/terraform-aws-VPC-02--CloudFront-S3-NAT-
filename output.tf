901c411949017cb6aa7b11f1964a3545ca179a6b
Viet <dstijl62@gmail.com>
2026-08-01 18:26:59 +0700

    1st

---
+++ output.tf
@@ -0,0 +1,15 @@
+
+# output "instance_ami" {
+#   value = aws_instance.web.ami
+# }
+
+# output "instance_state" {
+#   value = aws_instance.web.instance_state
+#   description = "Trang thai hien tai cua VM WEB"
+# }
+
+# output "public_ip" {
+#   value = aws_instance.web.public_ip
+#   sensitive = true
+# }
+
